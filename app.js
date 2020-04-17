c3868f92542af35954ddf68c468ba80f9259f5d0
amitgouda <amitgouda17995@gmai.com>
2020-04-18 03:52:10 +0530

    IMplemnetd auth and user routes

--- app.js
+++ app.js
@@ -8,13 +8,14 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
 
 //DB Connection
 mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
   })
   .then(() => {
     console.log("DB CONNECTED");
@@ -27,6 +28,7 @@ app.use(cors());
 
 //My Routes
 app.use("/api", authRoutes);
+app.use("/api", userRoutes);
 
 //PORT
 const port = process.env.PORT || 8000;
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,14 @@
 const User = require("../models/user");
 const { check, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const expressJwt = require("express-jwt");
 
 exports.signup = (req, res) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
     return res.status(422).json({
-      error: errors.array()[0].msg
+      error: errors.array()[0].msg,
     });
   }
 
@@ -14,19 +16,80 @@ exports.signup = (req, res) => {
   user.save((err, user) => {
     if (err) {
       return res.status(400).json({
-        err: "NOT able to save user in DB"
+        err: "NOT able to save user in DB",
       });
     }
     res.json({
       name: user.name,
       email: user.email,
-      id: user._id
+      id: user._id,
     });
   });
 };
 
 exports.signout = (req, res) => {
+  res.clearcookie("token");
   res.json({
-    message: "User signout"
+    message: "User signout",
   });
+};
+
+exports.signin = (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg,
+    });
+  }
+
+  const { email, password } = req.body;
+
+  User.findOne({ email }, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "USER email does not exist",
+      });
+    }
+
+    if (!user.autheticate(password)) {
+      return res.status(401).json({
+        error: "Email and password do not match",
+      });
+    }
+
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+    //put token in cookie
+    res.cookie("token", token, { expire: new Date() + 9999 });
+
+    const { _id, name, email, role } = user;
+
+    return res.status(200).json({ token, user: { _id, name, email, role } });
+  });
+};
+
+// protect routes
+exports.isSignedIn = expressJwt({
+  secret: process.env.SECRET,
+  userProperty: "auth",
+});
+
+//custome middlewares
+exports.isAuthenticated = (req, res, next) => {
+  let checker = req.profile && req.auth && req.profile._id == req.auth._id;
+  if (!checker) {
+    return res.status(403).json({
+      error: "ACCESS DENIED",
+    });
+  }
+  next();
+};
+
+exports.isAdmin = (req, res, next) => {
+  if (req.profile.role === 0) {
+    return res.status(403).json({
+      error: "You are not ADMIN,access denied",
+    });
+  }
+  next();
 };
